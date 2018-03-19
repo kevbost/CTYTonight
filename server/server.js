9f9e30b15a2d21f6b4de51c1a2ad03896af4e119
Kevin Bost <kevbost@gmail.com>
2018-03-18 20:26:37 -0400

    enable babel for building express

--- server/server.js
+++ server/server.js
@@ -1,4 +1,17 @@
-const express = require( 'express' )
+// const express = require( 'express' )
+
+// const app = express()
+// const port = process.env.PORT || 5000
+
+// app.get( '/api/hello', ( req, res ) => {
+//   res.json({
+//     express: 'Hello From ${WORKSPACE_ROOT}/server/server.js'
+//   })
+// })
+
+// app.listen( port, () => { return console.log( `Listening on port ${port}` ) }) // eslint-disable-line
+
+import express from 'express'
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,4 +22,6 @@ app.get( '/api/hello', ( req, res ) => {
   })
 })
 
-app.listen( port, () => { return console.log( `Listening on port ${port}` ) }) // eslint-disable-line
+app.listen( port, () => {
+  return console.log( `Listening on port ${port}` )  // eslint-disable-line
+})
